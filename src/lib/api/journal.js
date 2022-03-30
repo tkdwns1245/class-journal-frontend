4376aa5b4b9eb17a76f74845b402052087bbfc89
SANGJUN SON <tkdwns1245@gmail.com>
2022-03-30 23:00:07 +0900

    change api url

--- src/lib/api/journal.js
+++ src/lib/api/journal.js
@@ -2,9 +2,9 @@ import client from './client';
 
 export const register = {
     journal : ({schoolName,gradeNum,classroomNum,themeColor,createDate}) =>
-            client.post('/api/journal/register',{schoolName,gradeNum,classroomNum,themeColor,createDate}),
+            client.post('/api/journal/',{schoolName,gradeNum,classroomNum,themeColor,createDate}),
     memo : ({title,content,selectedMonth,selectedJournal}) =>
-            client.post('/api/journal/memo/register',{title,content,selectedMonth,selectedJournal}),
+            client.post('/api/journal/memo/',{title,content,selectedMonth,selectedJournal}),
 }
 
 export const update = {
@@ -22,11 +22,11 @@ export const remove = {
 }
 
 export const listJournals = ({}) =>
-        client.get('/api/journal/list',{});
+        client.get('/api/journal/',{});
 export const listMemos = ({journal_id,selectedMonth}) => {
         const params = {
                 journal_id,
                 selectedMonth
         };
-        return client.get('/api/journal/memo/list',{params});
+        return client.get('/api/journal/memo/',{params});
 }
